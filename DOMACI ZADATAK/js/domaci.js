43019b8a75b2293d0d1060a1b9f2abe9fabcc83a
bokicaM <112815859+bokicaM@users.noreply.github.com>
2022-10-18 13:45:46 +0200

    Delete DOMACI ZADATAK directory

--- DOMACI ZADATAK/js/domaci.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('html').on('click', function (e) {
-        if (e.target.id == 'dropdown') {
-            $("#dropdown-menu").toggle();
-        } else {
-            $("#dropdown-menu").hide();
-        }
-    });
-});
--- DOMACI ZADATAK/js/validacija.js
+++
@@ -1,8 +0,0 @@
-function validacija() {
-    let x = document.forms["forma"]["brTel"].value;
-    let y = document.forms["forma"]["brRez"].value;
-    let len = {min: 6, max: 60}
-    if (x == "" || y == "") {
-        alert("Molimo vas da popunite sva polja!");
-    }
-}
